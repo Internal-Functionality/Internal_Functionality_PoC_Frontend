9c17f83451d59eccf06167df4ac5fd3ea1b03d87
CrhistoferVera <cristofervera110@gmail.com>
2025-10-02 22:00:02 -0400

    add home page

---
+++ src/app/act-fixers-demo/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page (){
+  return (
+    <div>
+        <h1>Demo actividad fixers</h1>
+    </div>
+  )
+}
---
+++ src/app/act-requesters-hu6/page.tsx
@@ -0,0 +1,8 @@
+
+export default function page (){
+    return(
+        <div>
+            <h1>Demo actividad requesters hu6</h1>
+        </div>
+    )
+}
