d394d8a8c6f96b3ad91cffbcdae06cda4bc45fe8
Mayra <mayvillarroel2005@gmail.com>
2025-10-07 18:47:01 -0400

    add JobRegisterBox component

--- src/app/act-requesters-hu6/page.tsx
+++ src/app/act-requesters-hu6/page.tsx
@@ -1,6 +1,6 @@
 import FixerCard from "@/components/components-h6/FixerCard";
 import JobOffersBox from "@/components/components-h6/JobOffersBox";
-
+import JobRegisterBox from "@/components/components-h6/JobRegisterBox";
 export default function page() {
     return (
         <> 
@@ -12,6 +12,9 @@ export default function page() {
         <div className="flex space-x-9 ">
             <div className="max-w-2xl w-full p-6">
                 <JobOffersBox />
+            </div>
+            <div className="max-w-2xl w-full p-6">
+                <JobRegisterBox />
             </div>
         </div>
         </>
---
+++ src/components/components-h6/JobRegisterBox.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function JobRegisterBox(){
+    return (
+        <div className="max-w-2xl w-full text-2xl mt-5">
+            Trabajos Realizados:
+            <p className="text-sm mt-2 ml-1">Total 2 trabajos</p>
+            <ul className="text-sm text-left">
+                <li className="w-full mt-8">
+                    <Link href="" className="block bg-blue-100 p-5 border border-black rounded-t-xl w-full text-2xl">
+                        Reparacion PC
+                        <p className="text-sm mt-3">Comentario de Maria Lopez: Excelente trabajo, lo areglo de manera
+                        precisa y rapida</p>
+                    </Link>
+                </li>
+                <li className="w-full">
+                    <Link href="" className="block bg-blue-100 p-5 border border-black w-full text-2xl rounded-b-xl">
+                    Instalación de programa                        
+                    <p className="text-sm mt-3">Comentario de Juan Perez: No me gusto el trabajo, el sistema tiene
+                    muchas fallas</p>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
