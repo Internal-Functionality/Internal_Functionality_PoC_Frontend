cf3e791fee9e0a5655f35cd22ab77ba2b72b2792
svfabio <svfabio.andres@gmail.com>
2025-10-24 00:57:53 -0400

    Change menu's font and change their colors

--- src/components/components-h5/Header.tsx
+++ src/components/components-h5/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ userName }) => {
   return (
     <div className="w-full p-6 bg-blue-100 rounded-xl shadow-md flex items-center space-x-6">
-      <div className="w-16 h-16 bg-blue-300 rounded-full flex items-center justify-center text-white font-bold text-2xl">
+      <div className="w-16 h-16 bg-[#759AE0] rounded-full flex items-center justify-center text-white font-bold text-2xl">
         F
       </div>
       <div>
